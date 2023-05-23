f9d256ab06d8a988105ae50a06fd93615ff51871
Isseki WATABE <oneseki743@gmail.com>
2023-05-24 00:28:00 +0900

    update

--- test.cpp
+++ test.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main() {
-  cout << "いつも2525" << endl;
-  cout << "AtCoderくん" << endl;
-};
+  long long N, W;
+  cin >> N >> W;
+  vector<pair<long long, long long>> V(N);
+  for (auto &v : V) {
+    cin >> v.first >> v.second;
+  }
 
+  sort(v.begin(), v.end());
+  reverse(v.begin(), v.end());
+
+  long long ans = 0;
+  for (auto &v : V) {
+    ans += v.first * min(W, v.second);
+    W -= min(W, v.second);
+  }
+  cout << ans << endl;
+  return 0;
+}
