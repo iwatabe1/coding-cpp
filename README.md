2ad110423eabdb1df5015e339cbc3c68a6c52b7a
Isseki WATABE <oneseki743@gmail.com>
2023-05-28 18:25:39 +0900

    update launch settings

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # coding-cpp
+
+# 2次元配列
+vector<vector<要素の型>> 変数名(要素数1, vector<要素の型>(要素数2, 初期値))
--- test.cpp
+++ test.cpp
@@ -1,22 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// vector<vector<要素の型>> 変数名(要素数1, vector<要素の型>(要素数2, 初期値))
+
 int main() {
-  long long N, W;
-  cin >> N >> W;
-  vector<pair<long long, long long>> V(N);
-  for (auto &v : V) {
-    cin >> v.first >> v.second;
+  int N, S;
+  cin >> N >> S;
+  vector<int> A(N), P(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A.at(i);
+  }
+  for (int i = 0; i < N; i++) {
+    cin >> P.at(i);
   }
 
-  sort(v.begin(), v.end());
-  reverse(v.begin(), v.end());
+  // リンゴ・パイナップルをそれぞれ1つずつ購入するとき合計S円になるような買い方が何通りあるか
+  // ここにプログラムを追記
 
-  long long ans = 0;
-  for (auto &v : V) {
-    ans += v.first * min(W, v.second);
-    W -= min(W, v.second);
+  int ans = 0;
+  for (int a : A) {
+    for (int p : P) {
+      if (S == (a + p)) {
+        ans++;
+      }
+    }
   }
+
   cout << ans << endl;
-  return 0;
 }
