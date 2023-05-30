3894ff8f075b55958ed06b1b8580504825eccba3
Isseki WATABE <oneseki743@gmail.com>
2023-05-31 00:17:48 +0900

    update

--- test.cpp
+++ test.cpp
@@ -1,30 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// vector<vector<要素の型>> 変数名(要素数1, vector<要素の型>(要素数2, 初期値))
+// x番の組織について、子組織からの報告書が揃った時刻を返す
+// childrenは組織の関係を表す2次元配列(参照渡し)
+int complete_time(vector<vector<int>> &children, int x) {
+  if (children.at(x).size() == 0) {
+    return 1;
+  }
+
+  int sum = 0;
+  for (int child : children.at(x)) {
+    sum += complete_time(children, child);
+  }
+  sum++;
+  return sum;
+}
+
+// これ以降の行は変更しなくてよい
 
 int main() {
-  int N, S;
-  cin >> N >> S;
-  vector<int> A(N), P(N);
-  for (int i = 0; i < N; i++) {
-    cin >> A.at(i);
-  }
-  for (int i = 0; i < N; i++) {
-    cin >> P.at(i);
-  }
+  int N;
+  cin >> N;
 
-  // リンゴ・パイナップルをそれぞれ1つずつ購入するとき合計S円になるような買い方が何通りあるか
-  // ここにプログラムを追記
-
-  int ans = 0;
-  for (int a : A) {
-    for (int p : P) {
-      if (S == (a + p)) {
-        ans++;
-      }
-    }
+  vector<int> p(N);  // 各組織の親組織を示す配列
+  p.at(0) = -1;  // 0番組織の親組織は存在しないので-1を入れておく
+  for (int i = 1; i < N; i++) {
+    cin >> p.at(i);
   }
 
-  cout << ans << endl;
+  // 組織の関係から2次元配列を作る(理解しなくてもよい)
+  vector<vector<int>> children(
+      N);  // ある組織の子組織の番号一覧  // N×0の二次元配列
+  for (int i = 1; i < N; i++) {
+    int parent = p.at(i);              // i番の親組織の番号
+    children.at(parent).push_back(i);  // parentの子組織一覧にi番を追加
+  }
+
+  // 0番の組織の元に報告書が揃う時刻を求める
+  for (int i = 0; i < N; ++i) {
+    cout << complete_time(children, i) << endl;
+  }
 }
